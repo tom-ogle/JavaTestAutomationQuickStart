25a94762eabe5e17492069a17f5de40b478d322c
Tom Ogle <t.ogle.uk@gmail.com>
2015-09-02 20:10:20 +0100

    Removed unused import.

--- src/test/java/mycompany/tests/W3Test.java
+++ src/test/java/mycompany/tests/W3Test.java
@@ -2,7 +2,6 @@ package mycompany.tests;
 
 
 import mycompany.Library;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
